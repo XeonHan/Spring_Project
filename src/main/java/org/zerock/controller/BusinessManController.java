afd36db6496f88ee778e95756be4f8ac0ba21421
lygnis <89439371+lygnis@users.noreply.github.com>
2023-12-26 15:31:50 +0900

    feat Service

--- src/main/java/org/zerock/controller/BusinessManController.java
+++ src/main/java/org/zerock/controller/BusinessManController.java
@@ -1,9 +1,13 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.quotation.QuotationVO;
+import org.zerock.service.business.QuotationService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -13,10 +17,11 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/business/*")
 @AllArgsConstructor
 public class BusinessManController {
+	private QuotationService service;
 	
 	@GetMapping("/quoteInquiry")
 	public void quoteInquiry(Model model) {
-		
-		
+		List<QuotationVO> data= service.readList();
+		log.info(data);
 	}
 }
