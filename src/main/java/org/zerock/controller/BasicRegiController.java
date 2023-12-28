fbf6a64e847139a50fa37e9650c16b90e3e8ba0c
KimHyunMin0908 <khm980908@gmail.com>
2023-12-28 10:56:31 +0900

    ItemList.jsp

--- src/main/java/org/zerock/controller/BasicRegiController.java
+++ src/main/java/org/zerock/controller/BasicRegiController.java
@@ -1,12 +1,15 @@
 package org.zerock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.PageDTO;
@@ -44,8 +47,10 @@ public class BasicRegiController {
 	}
 
 	@GetMapping("/itemGet")
-	public void itemGet(@RequestParam("item_code") Integer item_code, Model model) {
-		model.addAttribute("itemList", itemService.get(item_code));
+	@ResponseBody
+	public ResponseEntity<ItemVO> itemGet(@RequestParam("item_code") Integer item_code) {
+		ItemVO item = itemService.get(item_code);
+		return new ResponseEntity<>(item, HttpStatus.OK);
 	}
 
 	@PostMapping("/itemModify")
