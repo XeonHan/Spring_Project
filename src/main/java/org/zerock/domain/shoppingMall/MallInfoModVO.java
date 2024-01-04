a49cf22db217b40382b9cc7023ad96bbd84743e3
LHKL321 <59049045+LHKL321@users.noreply.github.com>
2024-01-04 10:39:10 +0900

    추가

---
+++ src/main/java/org/zerock/domain/shoppingMall/MallInfoModVO.java
@@ -0,0 +1,16 @@
+package org.zerock.domain.shoppingMall;
+
+import lombok.Data;
+
+@Data
+public class MallInfoModVO {
+	private String shop_Code;
+	private String shop_Name;
+	private String mall_ID;
+	private String password;
+	private String auth_Code;
+	private String comp_Name;
+	private String site_Type;
+
+
+}
